7d20bde384777543d44d08877501ef6b0e91cd32
Yash Kamothi <ykamo001@hammer.cs.ucr.edu>
2015-04-13 11:05:07 -0700

    added extra credit features

--- main.cpp
+++ main.cpp
@@ -3,11 +3,17 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
 
 using namespace std;
 
 int main()
 {
-	
+	string user = getlogin();	//gets the username and stores it in string user
+	char name[100];				//initialize emptry string to be used to get the host name
+	gethostname(name, 100);		//gets the host's name and stores it in the char array variable name
+	cout << user << "@" << name << "$" << endl;	//output user name, @ symbol, the host name, followed by $
+												// output at this point should result in: [user]@[host]$
 	return 0;
 }
