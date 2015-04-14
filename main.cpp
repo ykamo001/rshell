32f2c10fa8bab753440a29692f9587842308b7d7
Yash Kamothi <ykamo001@hammer.cs.ucr.edu>
2015-04-13 18:36:34 -0700

    implemented exit option and created while loop

--- main.cpp
+++ main.cpp
@@ -13,7 +13,18 @@ int main()
 	string user = getlogin();	//gets the username and stores it in string user
 	char name[100];				//initialize emptry string to be used to get the host name
 	gethostname(name, 100);		//gets the host's name and stores it in the char array variable name
-	cout << user << "@" << name << "$" << endl;	//output user name, @ symbol, the host name, followed by $
-												// output at this point should result in: [user]@[host]$
+	
+	bool done = false;
+	while(!done)
+	{
+		cout << user << "@" << name << " $ ";	//output user name, @ symbol, the host name, followed by $
+							// output at this point should result in: [user]@[host]$
+		string command;
+		getline(cin, command);
+		if(command == "exit")
+		{
+			done = true;
+		}
+	}
 	return 0;
 }
