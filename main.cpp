fa0a38fc023e42aab0e95cef27122820c7efe6d5
Yash Kamothi <ykamo001@hammer.cs.ucr.edu>
2015-04-13 20:07:32 -0700

    added parsing for semicolon

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include <sys/wait.h>
 #include <stdio.h>
 #include <string.h>
+#include <cstring>
+#include <vector>
+#include <boost/algorithm/string/trim.hpp>
 
 using namespace std;
 
@@ -15,15 +18,37 @@ int main()
 	gethostname(name, 100);		//gets the host's name and stores it in the char array variable name
 	
 	bool done = false;
+	string command;
+	char *token;
+	vector<char*> library;
+	vector<string> sc_cmd;
 	while(!done)
 	{
 		cout << user << "@" << name << " $ ";	//output user name, @ symbol, the host name, followed by $
 							// output at this point should result in: [user]@[host]$
-		string command;
-		getline(cin, command);
+		getline(cin, command);				//get the entire line of command from user
 		if(command == "exit")
 		{
 			done = true;
+		}
+		else
+		{
+			char cmd[command.size()];
+			strcpy(cmd, command.c_str());
+			token = strtok(cmd, ";");
+			while(token != NULL)
+			{
+				library.push_back(token);
+				token = strtok(NULL, ";");
+			}
+			for(int i = 0; i < library.size(); ++i)
+			{
+				sc_cmd.push_back(string(library.at(i)));
+			}
+			for(int i = 0; i < sc_cmd.size(); ++i)
+			{
+				cout << sc_cmd.at(i) << endl;
+			}
 		}
 	}
 	return 0;
