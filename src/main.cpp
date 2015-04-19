d394d634a3795a418d127658c4b6257e6138c2cb
Yash Kamothi <ykamo001@hammer.cs.ucr.edu>
2015-04-19 00:04:33 -0700

    fixed comment bug in command line

--- src/main.cpp
+++ src/main.cpp
@@ -37,126 +37,147 @@ int main()
 		}
 		else
 		{
-			char cmd[command.size()];
-			strcpy(cmd, command.c_str());		//make the inputted command into a c string so that we can parse
-			token = strtok(cmd, ";");
-			while(token != NULL)
+			char* cmd = new char[command.size()];
+			//strcpy(cmd, command.c_str());		//make the inputted command into a c string so that we can parse
+			//token = strtok(cmd, "#");					//parse the string looking for the start of a comment
+			//if(token != NULL)		//then only add the beginning part of the command and throw away everything after the #
+			//{
+			//	if(string(token).find_first_not_of(' ') != std::string::npos) //if the parsed string is only white spaces
+			//	{									//then dont add it list of commands
+			//		copy.push_back(string(token));	
+			//	}
+			//}
+			unsigned int loc = command.find_first_of("#");
+			command = command.substr(0, loc);
+			if(command.size() > 0)
 			{
-				sc_cmd.push_back(string(token));		//parse the string, looking for semicolons to split the commands
-				token = strtok(NULL, ";");
+				copy.push_back(command);
 			}
-			for(int i = 0; i < sc_cmd.size(); ++i)
+			if(copy.size() > 0)
 			{
-				strcpy(cmd, (sc_cmd.at(i)).c_str());
-				token = strtok(cmd, "#");					//parse the string looking for the start of a comment
-				if(token != NULL)		//then only add the beginning part of the command and throw away everything after the #
-				{
-					if(string(token).find_first_not_of(' ') != std::string::npos) //if the parsed string is only white spaces
-					{									//then dont add it list of commands
-						copy.push_back(string(token));	
-					}
-				}
-			}
-			sc_cmd.clear();
-			for(int i = 0; i < copy.size(); ++i)
-			{
-				sc_cmd.push_back(copy.at(i));			//move the new filtered commands into the proper vector
-			}
-			copy.clear();
-			for(int i = 0; i < sc_cmd.size(); ++i)		//keep executing these commands regardless of result from previous command
-			{
-				trim(sc_cmd.at(i));					//remove all the unnecessary white spaces around the string if in there
-				strcpy(cmd, (sc_cmd.at(i)).c_str());
-				token = strtok(cmd, "||");
+				strcpy(cmd, (copy.at(0)).c_str()); 
+				copy.clear();
+				token = strtok(cmd, ";");
 				while(token != NULL)
 				{
-					or_cmd.push_back(string(token));		//parse the string for || commands and split the code down to && commnds
-					token = strtok(NULL, "||");
+					sc_cmd.push_back(string(token));		//parse the string, looking for semicolons to split the commands
+					token = strtok(NULL, ";");
 				}
-				int fails = 0;
-				for(int i = 0; i < or_cmd.size(); ++i)			//keep doing || commands as long as the last command failed
+				for(unsigned int i = 0; i < sc_cmd.size(); ++i)
 				{
-					trim(or_cmd.at(i));
-					if(fails == i)
+					strcpy(cmd, (sc_cmd.at(i)).c_str());
+					token = strtok(cmd, "#");					//parse the string looking for the start of a comment
+					if(token != NULL)		//then only add the beginning part of the command and throw away everything after the #
 					{
-						strcpy(cmd, (or_cmd.at(i)).c_str());
-						token = strtok(cmd, "&&");
-						while(token != NULL)			//parse the string command for && and split the code down to single commands
-						{
-							and_cmd.push_back(string(token));
-							token = strtok(NULL, "&&");
-						}
-						int trues = 0;
-						for(int i = 0; i < and_cmd.size(); ++i)		//keep doing && commands as long as the last command succeeded
-						{
-							if(trues == i)
-							{
-								strcpy(cmd, (and_cmd.at(i)).c_str());
-								token = strtok(cmd, " ");
-								while(token != NULL)			//break down the command into strings
-								{
-									copy.push_back(string(token));
-									token = strtok(NULL, " ");
-								}
-								char* argv[copy.size()+1];		//create an array of char pointers
-								for(int j = 0; j < copy.size(); ++j)
-								{
-									trim(copy.at(j));		//add char pointers into an array of pointers
-									argv[j] = const_cast<char*>((copy.at(j)).c_str());	//this will allow us to use execvp 
-								}
-								argv[copy.size()] = 0;
-								int pid = fork();
-								if(pid == -1)
-								{
-									perror("There was an error with fork(). ");
-								}
-								else if(pid == 0)
-								{
-									if(and_cmd.at(i) != "exit")	//only move along to execvp if the command is not exit
-									{
-										if(-1 == execvp((copy.at(0)).c_str(), argv))
-										{
-											perror("There was an error with execvp(). ");	
-											_exit(1);		//if the command failed, then kill the child process and exit
-										}
-									}
-									else		//if the command is to exit, dont do anything, simply kill the child process and exit
-									{
-										_exit(1);
-									}
-								}
-								else
-								{
-									int status;
-									wait(&status);
-									if(status == -1)
-									{
-										perror("There was en error with wait(). ");
-									}
-									else if(status == 0)		//if the last command was executed successfully, increase the counter
-									{
-										++trues;
-									}
-									if(and_cmd.at(i) == "exit")		//if the user entered the exit command, end the shell and program
-									{
-										done = true;
-										return 0;
-									}
-								}
-							}
-							copy.clear();				//clear out the vector to be used again
-						}									//end of and_cmd
-						if(trues != and_cmd.size())
-						{
-							fails++;			//if the amount of commands that were successfully executed is not the same as the
-												//number of commands executed, then increase the number of commands failed
+						if(string(token).find_first_not_of(' ') != std::string::npos) //if the parsed string is only white spaces
+						{									//then dont add it list of commands
+							copy.push_back(string(token));	
 						}
 					}
-					and_cmd.clear();			//clear out the commands in and vector since we are done
-				}						//end of or_cmd
-				or_cmd.clear();			//clear out the commands in the or vector since we are done
-			}			//end of sc_cmd
+				}
+				sc_cmd.clear();
+				for(unsigned int i = 0; i < copy.size(); ++i)
+				{
+					sc_cmd.push_back(copy.at(i));			//move the new filtered commands into the proper vector
+				}
+				copy.clear();
+				for(unsigned int i = 0; i < sc_cmd.size(); ++i)	//keep executing these commands regardless of result from previous command
+				{
+					trim(sc_cmd.at(i));					//remove all the unnecessary white spaces around the string if in there
+					strcpy(cmd, (sc_cmd.at(i)).c_str());
+					token = strtok(cmd, "||");
+					while(token != NULL)
+					{
+						or_cmd.push_back(string(token));		//parse the string for || commands and split the code down to && commnds
+						token = strtok(NULL, "||");
+					}
+					unsigned int fails = 0;
+					for(unsigned int i = 0; i < or_cmd.size(); ++i)			//keep doing || commands as long as the last command failed
+					{
+						trim(or_cmd.at(i));
+						if(fails == i)
+						{
+							strcpy(cmd, (or_cmd.at(i)).c_str());
+							token = strtok(cmd, "&&");
+							while(token != NULL)			//parse the string command for && and split the code down to single commands
+							{
+								and_cmd.push_back(string(token));
+								token = strtok(NULL, "&&");
+							}
+							unsigned int trues = 0;
+							for(unsigned int i = 0; i < and_cmd.size(); ++i)//keep doing && commands as long as the last command succeeded
+							{
+								if(trues == i)
+								{
+									strcpy(cmd, (and_cmd.at(i)).c_str());
+									token = strtok(cmd, " ");
+									while(token != NULL)			//break down the command into strings
+									{
+										copy.push_back(string(token));
+										token = strtok(NULL, " ");
+									}
+									char **argv = new char*[copy.size()+1];		//create an array of char pointers
+									for(unsigned int j = 0; j < copy.size(); ++j)
+									{
+										trim(copy.at(j));		//add char pointers into an array of pointers
+										argv[j] = const_cast<char*>((copy.at(j)).c_str());	//this will allow us to use execvp 
+									}
+									argv[copy.size()] = 0;
+									int pid = fork();
+									if(pid == -1)
+									{
+										perror("There was an error with fork(). ");
+									}
+									else if(pid == 0)
+									{
+										if(and_cmd.at(i) != "exit")	//only move along to execvp if the command is not exit
+										{
+											if(-1 == execvp((copy.at(0)).c_str(), argv))
+											{
+												perror("There was an error with execvp(). ");	
+												_exit(1);		//if the command failed, then kill the child process and exit
+											}
+										}
+										else		//if the command is to exit, dont do anything, simply kill the child process and exit
+										{
+											_exit(1);
+										}
+									}
+									else
+									{
+										int status;
+										wait(&status);
+										if(status == -1)
+										{
+											perror("There was en error with wait(). ");
+										}
+										else if(status == 0)		//if the last command was executed successfully, increase the counter
+										{
+											++trues;
+										}
+										if(and_cmd.at(i) == "exit")		//if the user entered the exit command, end the shell and program
+										{
+											done = true;
+											return 0;
+										}
+									}
+								delete []argv;
+								}
+								copy.clear();				//clear out the vector to be used again
+							}									//end of and_cmd
+							if(trues != and_cmd.size())
+							{
+								fails++;			//if the amount of commands that were successfully executed is not the same as the
+													//number of commands executed, then increase the number of commands failed
+							}
+						}
+						and_cmd.clear();			//clear out the commands in and vector since we are done
+					}						//end of or_cmd
+					or_cmd.clear();			//clear out the commands in the or vector since we are done
+				}			//end of sc_cmd
 			sc_cmd.clear();			//clear out the commands in the semicolon vector since we are done
+			}
+		delete []cmd;
 		}
 	}				//end of while(!done) loop
 	return 0;
