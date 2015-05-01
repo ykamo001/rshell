bb552c7c6ff14af776e68b5ead27d0f17ae46cb4
Yash Kamothi <ykamo001@hammer.cs.ucr.edu>
2015-05-01 00:16:46 -0700

    added l flag

--- src/ls.cpp
+++ src/ls.cpp
@@ -13,6 +13,9 @@
 #include <cstdlib>
 #include <errno.h>
 #include <dirent.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
 
 using namespace std;
 using namespace boost;
@@ -75,7 +78,7 @@ void getfiles(vector<string> &currfiles, char** argv, const int &loc)	//this fun
 	errno = 0;
 	if(NULL == (currdir = opendir(argv[loc])))
 	{
-		perror("There was an error with opendir(). ");
+		perror("There was an error with opendir() ");
 		exit(1);
 	}
 	while(NULL != (files = readdir(currdir)))
@@ -84,12 +87,12 @@ void getfiles(vector<string> &currfiles, char** argv, const int &loc)	//this fun
 	}
 	if(errno != 0)
 	{
-		perror("There was an error with readdir(). ");
+		perror("There was an error with readdir() ");
 		exit(1);
 	}
 	if(-1 == closedir(currdir))
 	{
-		perror("There was an error with closedir(). ");
+		perror("There was an error with closedir() ");
 		exit(1);
 	}
 	sort(currfiles.begin(), currfiles.end(), compareNoCase);
@@ -117,6 +120,90 @@ void outputnorm(vector<string> &display)
 	cout << endl;
 }
 
+void withouthidden(const vector<string> &take, vector<string> &give)
+{
+	for(unsigned int i = 0; i < take.size(); ++i)
+	{
+		if(take.at(i).at(0) != '.')
+		{
+			give.push_back(take.at(i));
+		}
+	}
+	sort(give.begin(), give.end(), compareNoCase);
+}
+
+void outputl(vector<string> &files)
+{
+	struct passwd *userid;
+	struct group *groupid;
+	struct status;
+	for(unsigned int i = 0; i < files.size(); ++i)
+	{
+		if(-1 == (stat(files.at(i).c_str(), &status)))
+		{
+			perror("There was an error with stat() ");
+			exit(1);
+		}
+		else
+		{
+			if(S_IFDIR & status.st_mode)
+			{
+				cout << 'd';
+			}
+			else if(S_IFLNK & status.st_mode)
+			{
+				cout << 'l';
+			}
+			else
+			{
+				cout << '-';
+			}
+			cout << ((S_IRUSR & status.st_mode) ? 'r' : '-');
+			cout << ((S_IWUSR & status.st_mode) ? 'w' : '-');
+			cout << ((S_IXUSR & status.st_mode) ? 'x' : '-');
+			cout << ((S_IRGRP & status.st_mode) ? 'r' : '-');
+			cout << ((S_IWGRP & status.st_mode) ? 'w' : '-');
+			cout << ((S_IXGRP & status.st_mode) ? 'x' : '-');
+			cout << ((S_IROTH & status.st_mode) ? 'r' : '-');
+			cout << ((S_IWOTH & status.st_mode) ? 'w' : '-');
+			cout << ((S_IXOTH & status.st_mode) ? 'x' : '-');
+			cout <<	' ' << status.st_nlink << ' ';
+			if(NULL == (userid = getpwuid(status.st_uid)))
+			{
+				perror("There was an error with getpwuid() ");
+				exit(1);
+			}
+			else
+			{
+				cout << userid->pw_name << ' ';
+			}
+			if(NULL == (groupid = getgrgid(status.st_gid)))
+			{
+				perror("There was an error with getgrdid() ");
+				exit(1);
+			}
+			else
+			{
+				cout << groupid->gr_name << ' ';
+			}
+			cout << status.st_blocks << ' ';
+			struct tm *tm;
+			char timebuf[20];
+			if(NULL == (tm = localtime(&(status.st_mtime))))
+			{
+				perror("There was an error with localtime() ");
+				exit(1);
+			}
+			else
+			{
+				strftime(timebuf, 20, "%b %d %H:%M", tm);
+				cout << timebuf << ' ';
+			}
+			cout << files.at(i);
+		}
+	}
+}
+
 int main(int argc, char **argv)
 {
 	vector<string> commands;
@@ -129,32 +216,59 @@ int main(int argc, char **argv)
 	{
 		if(directories.size() > 0)	//if directories were specified, come here
 		{
-			if(!hasl && !hasR)	//if no l or R flag, simple case, do this
+			for(unsigned int i = 0; i < directories.size(); ++i)
 			{
-				for(unsigned int i = 0; i < directories.size(); ++i)
+				getfiles(dirfiles, argv, directories.at(i));
+				if(directories.size() > 1)
 				{
-					getfiles(dirfiles, argv, directories.at(i));
-					if(directories.size() > 1)
-					{
-						cout << commands.at(directories.at(i)-1) << ": " << endl;
-					}
-					outputnorm(dirfiles);
-					if(i < directories.size()-1)
-					{
-						cout << endl;
-					}
-					dirfiles.clear();
+					cout << commands.at(directories.at(i)-1) << ": " << endl;
 				}
+				
+				if(!hasl && !hasR)	//if no l or R flag, simple case, do this
+				{
+					outputnorm(dirfiles);
+				}
+				else if(hasl && !hasR)
+				{
+					if(hasa)
+					{
+						outputl(dirfiles);
+					}
+					else
+					{
+						vector<string> temp;
+						withouthidden(dirfiles, temp);
+						outputl(temp);
+					}
+				}
+				if(directories.size() > 1)
+				{
+					cout << commands.at(directories.at(i)-1) << ": " << endl;
+				}
+				dirfiles.clear();
 			}
 		}
 		else //if no directory was specified, implied current directory, manually pass in . directory
 		{
 			char *temp[1];
 			temp[0] = new char('.');
+			getfiles(dirfiles, temp, 0);
 			if(!hasl && !hasR) //if no l or R flag, simple case, do this
 			{
-				getfiles(dirfiles, temp, 0);
 				outputnorm(dirfiles);
+			}
+			else if(hasl && !hasR)
+			{
+				if(hasa)
+				{
+					outputl(dirfiles);
+				}
+				else
+				{
+					vector<string> temp;
+					withouthidden(dirfiles, temp);
+					outputl(temp);
+				}
 			}
 		}
 	}
